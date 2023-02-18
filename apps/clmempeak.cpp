f53e3b54278421c32cc77d2f8894e67f47942119
Justin Berger <j.david.berger@gmail.com>
2023-02-18 09:57:42 -0700

    Test constant memory

--- apps/clmempeak.cpp
+++ apps/clmempeak.cpp
@@ -12,7 +12,7 @@
 
 static std::string source = R"18e3c792b59(
 
-__kernel void memread(__global const float *inptr, __global float *ptr, uint access_stride)
+__kernel void memread(MODIFIER const float *inptr, __global float *ptr, uint access_stride)
 {
     uint id = get_global_id(0);
     float y = (float)((float)(id) * 1e-10);
@@ -52,56 +52,55 @@ int main(int argc, char **argv) {
             //CL_USAGE_FLAG(static),
     };
 
-    int width = 512, height = 512;
+    int width = 128, height = 128;
 
-    for(auto& k : usages) {
-      auto name = std::get<0>(k);
-      auto buffer = OCL::Buffer(BufferInfo_t('f', 1, width, height), std::get<1>(k));
-        std::vector<uint8_t> d;
-        d.resize(buffer.buffer_size());
-        for(auto&v : d) {
-            v = 0xce;
-        }
-        if(buffer.host_can_write())
-            buffer.write(d.data());
-
-	auto flags = buffer.b->getInfo<CL_MEM_FLAGS>();
-        printf("%s: (flags 0x%x) \n", name.c_str(), flags);
-	
-        if(buffer.host_can_read())
-        {
-            Timeit t("\tread", width * height * 4  / 1024. / 1024., "MBs");
-            do {
-                buffer.read(d.data());
-            } while (t.under(1));
-        }
-
-        if(buffer.host_can_write())
-        {
-            Timeit t("\twrite", width * height * 4 / 1024. / 1024., "MBs");
-            do {
+    for(auto& modifier : {"__global", "__constant"}) {
+        for (auto &k: usages) {
+            auto name = std::get<0>(k);
+            auto buffer = OCL::Buffer(BufferInfo_t('f', 1, width, height), std::get<1>(k));
+            std::vector<uint8_t> d;
+            d.resize(buffer.buffer_size());
+            for (auto &v: d) {
+                v = 0xce;
+            }
+            if (buffer.host_can_write())
                 buffer.write(d.data());
-            } while (t.under(1));
-        }
 
-        for(auto& m : memaccess) {
+            auto flags = buffer.b->getInfo<CL_MEM_FLAGS>();
+            printf("%s: (flags 0x%x) %s \n", name.c_str(), flags, modifier);
 
-            auto kernel = OCL::Kernel("memread", {buffer.cl_constants(), source }, {buffer, out_buffer});
-            kernel.kernel.setArg(2, m.second);
-
-            context->queue.enqueueNDRangeKernel(kernel.kernel, cl::NullRange, globalWIs, localSize);
-            context->queue.enqueueNDRangeKernel(kernel.kernel, cl::NullRange, globalWIs, localSize);
-            context->queue.finish();
-
-            {
-                Timeit t("\t" + m.first, width * height * 4 * (double) (globalWIs) / 1024. / 1024., "MBs");
+            if (buffer.host_can_read()) {
+                Timeit t("\tread", width * height * 4 / 1024. / 1024., "MBs");
                 do {
-                    context->queue.enqueueNDRangeKernel(kernel.kernel, cl::NullRange, globalWIs, localSize);
-                    context->queue.finish();
+                    buffer.read(d.data());
                 } while (t.under(1));
+            }
+
+            if (buffer.host_can_write()) {
+                Timeit t("\twrite", width * height * 4 / 1024. / 1024., "MBs");
+                do {
+                    buffer.write(d.data());
+                } while (t.under(1));
+            }
+
+            for (auto &m: memaccess) {
+
+                auto kernel = OCL::Kernel("memread", {std::string("#define MODIFIER ") + modifier + "\n", buffer.cl_constants(), source}, {buffer, out_buffer});
+                kernel.kernel.setArg(2, m.second);
+
+                context->queue.enqueueNDRangeKernel(kernel.kernel, cl::NullRange, globalWIs, localSize);
+                context->queue.enqueueNDRangeKernel(kernel.kernel, cl::NullRange, globalWIs, localSize);
+                context->queue.finish();
+
+                {
+                    Timeit t("\t" + m.first, width * height * 4 * (double) (globalWIs) / 1024. / 1024., "MBs");
+                    do {
+                        context->queue.enqueueNDRangeKernel(kernel.kernel, cl::NullRange, globalWIs, localSize);
+                        context->queue.finish();
+                    } while (t.under(1));
+                }
             }
         }
     }
-
     return 0;
 }
